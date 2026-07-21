d88a006b5f1bd19d753b998f855959b49e6ef7b6
oneperc3ntfx-dotcom <oneperc3ntfx@gmail.com>
2026-07-22 02:19:49 +0700

    Update apps_script.py

--- apps_script.py
+++ apps_script.py
@@ -8,14 +8,32 @@ def save_member(data):
 
     try:
 
+        print("=== DATA KE SPREADSHEET ===")
+        print(data)
+
+
         response = requests.post(
+
             APPSCRIPT_URL,
+
             json=data,
-            timeout=10
+
+            timeout=15
+
         )
 
 
-        return response.json()
+        print("STATUS:")
+        print(response.status_code)
+
+
+        print("RESPON APPS SCRIPT:")
+        print(response.text)
+
+
+
+        return response.text
+
 
 
     except Exception as e:
